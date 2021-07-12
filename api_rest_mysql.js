cc45d57634b66cc949966fd197ca8cea07248dc6
laplage <nfrancis2013@gmail.com>
2021-07-12 12:07:10 +0200

    Version Api MysQL

---
+++ api_rest_mysql.js
@@ -0,0 +1,69 @@
+//1-    Importation des modules
+    const express = require('express')
+    const mysql = require('mysql')
+
+//2-    Création de l'instance de connexion au serveur de base de données MySQL
+    const db =  mysql.createConnection({
+        host : 'localhost',
+        user : 'root',
+        password : '',
+        database : 'gestion_produits'
+    })
+//3-    Etablir la connexion via l'objet de connexion db
+    db.connect((err)=>{
+        if(err)
+            console.log('Erreur de connexion : ' + err.message)
+        else{
+            // cas de connexion
+            console.log('Connected !')
+        /**
+         *  Nous allons dans le module de connexion à la base de données
+         *  -   Créer notre API REST de gestion des catégories
+         *          -   GET
+         *          -   POST
+         *          -   PUT
+         *          -   DELETE
+         *  -   Création du router nommé CategorieRouter pour gérer les différentes routes
+         *  -   Démarrer notre serveur
+         */
+            //1-    Création de l'instance express (Serveur)
+                const app = express()
+            //2-    Les variables globales
+                let PORT_NUMBER = 8086
+                    URL_ROOT = '/api/v1/categories'
+                //Router
+                let CategorieRouter = express.Router()
+
+            //3-    Les middlewares
+
+            //4-    Les différentes routes pour gerer les fonctions http (GET,POST,PUT et DELETE)
+                CategorieRouter.route('/:id')
+                    //Cette fonction affiche une catégorie connaissant son id
+                    .get((req,res)=>{
+                        //création de la requête SQL
+                        db.query('SELECT * FROM categories WHERE id = ?',[req.params.id],(err,data)=>{
+                            if(err)
+                                res.send('Erreur d\'exécution de la requête SQL')
+                            else
+                                res.send(data[0])
+                        })
+                    })
+                CategorieRouter.route('/')
+                    .get((req,res)=>{
+                        //Création de la requête SQL
+                        db.query('SELECT * FROM categories',(err,data)=>{
+                            if(err)
+                                res.send('Erreur d\'exécution de la requête SQL ')
+                            else
+                                res.send(data)
+                        })
+                    })
+            app.use(URL_ROOT,CategorieRouter) // cette instruction permet de lier le routeur CategorieRouter à une URL
+            //n-    Demarrage du serveur
+                app.listen(PORT_NUMBER,()=>{
+                    console.log('Server running on port : ' + PORT_NUMBER + ' http://localhost:'+PORT_NUMBER)
+                })
+            //n+1   Autres fonctions
+
+        }
+    })
--- app_mysql.js
+++ app_mysql.js
@@ -19,5 +19,61 @@
             console.log('Erreur ' + err.message)
         }else{
             console.log('Connected ! ')
+            /* Test des requêtes :
+                -   Afficher la liste des catégories (SELECT)
+                -   Afficher une catégorie précise connaissant son id (SELECT et WHERE) 
+                -   Rechercher les catégories commancant par une lettre ou contenant un mot ou une lettre (SELECT et LIKE)
+                -   Ajout d'une nouvelle catégorie (INSERT INTO)
+                -   Suppression d'une catégorie (DELETE)
+            */      
+           
+                //  Récupération de la liste de toutes les catégories
+                db.query('SELECT * FROM categories',(err,data)=>{
+                    if(err){
+                        console.log('Erreur : ' + err.message)
+                    }else{
+                        console.log(data)
+                    }
+                })
+                let idCat = 3;
+                //  Récupération  d'une catégorie précise connaissant son id (Passage de paramètre à la requête SQL)
+                db.query('SELECT * FROM categories WHERE id = ? ',[idCat],(err,data)=>{
+                    if(err){
+                        console.log('Erreur : ' + err.message)
+                    }else{
+                        console.log(data[0])
+                    }
+                })
+                // //  Test de l'ajout d'une catégorie 
+                // let id = 
+                //     nomCat = "VOITURES"
+                //     //INSERT INTO `categories`(`id`, `libelle`) VALUES ([value-1],[value-2])
+                // db.query('INSERT INTO categories(id,libelle) values(default,?)',[nomCat],(err)=>{
+                //     if(err)
+                //         console.log("Erreur d'insertion " + err.message)
+                //     else
+                //         console.log('Insertion effectuée avec succes')
+                // })
+                // //  Cas avec plusieurs paramètres : Mise à jour
+                // let id = 5
+                //     nouveauNom = "VELOS"
+                //     //UPDATE `categories` SET `id`=[value-1],`libelle`=[value-2] WHERE 1
+                //     db.query('UPDATE categories SET libelle = ? WHERE id = ?',[nouveauNom,id],(err)=>{
+                //         if(err)
+                //             console.log('Erreur de Mise-à-jour ' + err.message)
+                //         else    
+                //             console.log('Mise-à-jour effectuée avec succes')
+                //     })
+                // La suppression d'une catégorie dans la base de données
+                let id = 5
+                //DELETE FROM `categories` WHERE 0
+                db.query('DELETE FROM categories WHERE id = ? ',[id],(err)=>{
+                    if(err)
+                        console.log('Erreur de suppression '+ err.message)
+                    else    
+                        console.log('Catégorie supprimée avec succes')
+
+                })
+
         }
     })
