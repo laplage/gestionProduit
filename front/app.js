2ffdd7f6e664186878b002232e586090f1163d2c
laplage <nfrancis2013@gmail.com>
2021-07-13 15:35:03 +0200

    Version Api MysQL Axios

--- front/app.js
+++ front/app.js
@@ -5,36 +5,42 @@
  */
 //1-    Importation des modules
     const express = require('express')
-    var twig = require('twig')
-
+    const twig = require('twig')
+    const axios = require('axios')
 //2-    Les variables globales
     const app = express()
     const PORT_NUMBER = 3000
 
-//3-    Les middlewares
-
-
-//4-    Les routes
-    app.get('/',(req,res)=>{
-        //La fonction sendFile prend a paramètre le chemin Absolu
-        //console.log(__dirname)
-        
-        //res.send('test du chemin Absolu : ' + __dirname)
-        //res.sendFile(__dirname+'/views/index.html')
-
-        //Utilisation de twig (moteur de template)
-       //res.render('index.twig',{nom : "Francis Laplage"})
-       //récupération d'un paramètre
-
-        let tabNom = ["Bart","Lisa","Omer","Maggie"]
-
-
-        //res.render('index.twig',{nom : req.params.nom})
-        res.render('index.twig',{ listeNom : tabNom})
-
+    const instance = axios.create({
+        baseURL :'http://localhost:8086/api/v1/'
     })
-    app.get('/:nom',(req,res)=>{
-        res.render('index.twig',{nom : req.params.nom})
+//3-    Les middlewares
+//4-    Les routes
+    //Route pour la liste des catégories
+    app.get('/categories',(req,res)=>{
+
+        axios.get('http://localhost:8086/api/v1/categories/')
+             .then((resultat)=>{
+
+                res.render('categories.twig',{listeCat : resultat.data})
+             })
+             .catch((err)=>{
+                res.send("Erreur " + err.message)
+             })
+    })
+    //Route pour la suppression d'une catégorie
+    app.get('/delete/:id',(req,res)=>{
+        instance({
+            method : 'delete',
+            url : '/categories/'+req.params.id
+        })
+        .then(()=>{
+            res.redirect('/categories')
+        })
+        .catch((err)=>{
+            res.send('Erreur ' + err.message)
+        })
+
     })
 
 //5-    Démarrage de l'instance ou du serveur conrrespondant au front
