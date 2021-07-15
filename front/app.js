6be7de8f03b13b7ff526a8eed74e4067f5f98aec
laplage <nfrancis2013@gmail.com>
2021-07-15 11:47:07 +0200

    Version Api MysQL Axios2

--- front/app.js
+++ front/app.js
@@ -23,7 +23,6 @@
 
         axios.get('http://localhost:8086/api/v1/categories/')
              .then((resultat)=>{
-
                 res.render('categories.twig',{listeCat : resultat.data})
              })
              .catch((err)=>{
@@ -65,8 +64,34 @@
                 res.render('error.twig',{errorMsg : err.message})
             })
         })
-
-
+    //Route pour la mise-à-jour d'une catégorie
+        app.get('/edit/:id',(req,res)=>{
+            //res.send("ID est de  : " + req.params.id)
+            //l'ID nous permet de récupérer les données de toute la catégorie dans la base
+            instance.get('/categories/'+req.params.id)
+                .then((resultat)=>{
+                    //Fait appel au formulaire de mise-à-jour de la catégorie
+                    res.render('form_update_categorie.twig',{listeCat : resultat.data})
+                })
+                .catch((err)=>{
+                    res.render('error.twig',{errorMsg : err.message})
+                })
+        })
+        app.post('/edit',(req,res)=>{
+            instance({
+                method : 'put',
+                url : '/categories/'+req.body.id,
+                data :{
+                    libelle : req.body.libelle
+                }
+            })
+            .then(()=>{
+                res.redirect('/categories')
+            })
+            .catch((err)=>{
+                res.render('error.twig',{errorMsg : err.message})
+            })
+        })
 //5-    Démarrage de l'instance ou du serveur conrrespondant au front
 app.listen(PORT_NUMBER,()=>{
     console.log('Server Front running on port : ' + PORT_NUMBER + " http://localhost:"+PORT_NUMBER)
